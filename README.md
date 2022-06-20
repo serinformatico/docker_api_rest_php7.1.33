2d0a106c3c4e54bd2f632f4a5342f723b77ebe59
Sergio Regalado Alessi <serdigital@live.com.ar>
2022-06-20 13:59:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker_api_rest_php7.1.33_nginx1.2_mysql5.7_redis5.2.2_xdebug2.9.8
+Proyecto dockerizado: APIRest en PHP v7.1.33 + NGINX v1.2.2 + MySQL v5.7 + REDIS v5.2.2 + XDEBUG v2.9.8
---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php
+    $titulo1 = "APIRest<br> NGINX 1.2.2 + PHP 7.1.33 + MySQL 5.7<br>";
+    $titulo2 = "REDIS 5.2.2 + XDEBUG 2.9.3<br>";
+    $titulo3 = "Hello word!";
+    echo $titulo1;
+    echo $titulo2;
+    echo $titulo3;
+?>
